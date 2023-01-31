62810359366f6e60a99147e8839903b7344157cd
musabsari <musabsari07@gmail.com>
2023-01-31 18:32:22 +0100

    customerdatabase commit

---
+++ Template/Template.py
@@ -0,0 +1,20 @@
+#3 main class ?
+
+class User:
+    def __init__(self):
+        pass
+class Bank:
+    def __init__(self):
+        pass
+    def check_balance(self):
+        pass
+    def deposit_money(self):
+        pass
+    def withdraw_money(self):
+        pass
+    def statement(self):
+        pass
+
+class Admin:
+    def __init__(self):
+        pass
