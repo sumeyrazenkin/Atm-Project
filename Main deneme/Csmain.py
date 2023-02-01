20f4602b0332ca96cc62e5acdaa841da7ee496a8
melikkekaya <112420252+melikkekaya@users.noreply.github.com>
2023-02-01 18:39:38 +0100

    update_CSmain_window_Melike

---
+++ Main deneme/Csmain.py
@@ -0,0 +1,36 @@
+import sys
+from PyQt5.QtWidgets import *
+from Ui_customer_main_window import *
+
+class CSMain(QMainWindow, Ui_customer_main_window):
+    def __init__(self):
+        super(CSMain, self).__init__()
+        self.setupUi(self)
+        self.balance = 0
+
+        self.update_balance_display()
+        self.amount = int(self.csmainwdw_spinbox_money.value())
+        
+        self.csmainwdw_btn_getcash.clicked.connect(self.get_cash)
+        self.csmainwdw_btn_deposit.clicked.connect(self.deposit)
+    
+    def update_balance_display(self):
+        self.csmainwdw_lbl_balanceshow.setText(f"{str(self.balance)} €")
+
+    def deposit(self):
+        self.balance += self.csmainwdw_spinbox_money.value()
+        self.csmainwdw_lbl_resultmessage.setText("Successful deposit to the account..")
+        self.update_balance_display()
+
+    def get_cash(self):
+        self.balance -= self.csmainwdw_spinbox_money.value()
+        self.csmainwdw_lbl_resultmessage.setText("Successful withdraw from the account..")
+        self.update_balance_display()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    csmainwindow = CSMain()
+    widget = QtWidgets.QStackedWidget()
+    widget.addWidget(csmainwindow)
+    widget.show()
+    sys.exit(app.exec_())
