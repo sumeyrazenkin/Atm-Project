2004e8e009aa4dd9d19cfe8f82639428afaba441
melikkekaya <112420252+melikkekaya@users.noreply.github.com>
2023-02-02 16:15:35 +0100

    deneme

--- Main deneme/Csmain.py
+++ Main deneme/Csmain.py
@@ -1,18 +1,21 @@
 import sys
 from PyQt5.QtWidgets import *
 from Ui_customer_main_window import *
+import json
 
 class CSMain(QMainWindow, Ui_customer_main_window):
     def __init__(self):
         super(CSMain, self).__init__()
         self.setupUi(self)
+        
         self.balance = 0
-
         self.update_balance_display()
         self.amount = int(self.csmainwdw_spinbox_money.value())
-        
+
         self.csmainwdw_btn_getcash.clicked.connect(self.get_cash)
         self.csmainwdw_btn_deposit.clicked.connect(self.deposit)
+        # self.csmainwdw_btn_statement.clicked.connect(self.add_statement)
+        # self.csmainwdw_lbl_CSinfo.text(self.show_CSinfo)
     
     def update_balance_display(self):
         self.csmainwdw_lbl_balanceshow.setText(f"{str(self.balance)} €")
@@ -44,6 +47,26 @@ class CSMain(QMainWindow, Ui_customer_main_window):
         else:
             self.csmainwdw_lbl_resultmessage.setStyleSheet("color: rgb(255, 0, 0);")
             self.csmainwdw_lbl_resultmessage.setText("Please enter a positif amount..")
+
+    # def add_statement(self):
+    #     with open ("Main deneme/1234.json","r") as file:  #daha sonra f stringle csID yazılacak.
+    #         data = json.load(file)
+    #     print(data["Customer_ID"])
+
+      
+    # def show_CSinfo(self):
+    #     with open ("Main deneme/1234.json","r") as file:  #daha sonra f stringle csID yazılacak.
+    #         data = json.load(file)
+    #     name = data["Name"]
+    #     csID = data["Customer_ID"]
+    #     return f"You've reached {name}'s acount. Customer ID: {csID}"
+        
+
+        
+        
+
+
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
